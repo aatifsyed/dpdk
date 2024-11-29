0c086f742521091a2792dda488baee13e78f94f2
Aatif Syed <0xaatif@gmail.com>
2024-11-29 00:00:41 +0000

    feat: use third-party rust code

--- app/test_hello_rust/main.c
+++ app/test_hello_rust/main.c
@@ -2,13 +2,29 @@
 #include <stdio.h>
 
 size_t len(const char *);
+char *concat(const char *, const char *);
+void free_buf(char *);
 
 int main(int argc, const char **argv)
 {
     size_t my_len = len("hello");
-    if (printf("%ld", my_len) == EOF)
+    if (printf("%ld\n", my_len) == EOF)
     {
         return 1;
+    }
+    char *hello_world = concat("hello", "world");
+    if (hello_world != NULL)
+    {
+        if (printf("%s\n", hello_world) == EOF)
+        {
+            free_buf(hello_world);
+            return 1;
+        }
+        free_buf(hello_world);
+    }
+    else
+    {
+        printf("failed to concat\n");
     }
     return 0;
 }
--- lib/hello_rust/lib.rs
+++ lib/hello_rust/lib.rs
@@ -1,5 +1,7 @@
 use std::ffi::{c_char, CStr};
 
+use seestr::{Buf, NulTerminated};
+
 /// # Safety
 /// - If non-null, `ptr` must point to a nul-terminated string.
 #[no_mangle]
@@ -9,3 +11,16 @@ pub unsafe extern "C" fn len(ptr: *const c_char) -> usize {
         false => CStr::from_ptr(ptr).to_bytes().len(),
     }
 }
+
+#[no_mangle]
+pub extern "C" fn concat(left: &NulTerminated, right: &NulTerminated) -> Option<Buf> {
+    Buf::try_with(left.len() + right.len(), |buf| {
+        let (left_dst, right_dst) = buf.split_at_mut(left.len());
+        left_dst.copy_from_slice(left);
+        right_dst.copy_from_slice(right);
+    })
+    .ok()
+}
+
+#[no_mangle]
+pub extern "C" fn free_buf(_: Option<Buf>) {}
